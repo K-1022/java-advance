ad73dc6f3750351076f84ce42791d483b5bad3a7
K-1022 <167005898+K-1022@users.noreply.github.com>
2024-07-14 18:40:45 +0900

    応用問題2

--- java_advance/Advance1.java
+++ java_advance/Advance1.java
@@ -6,9 +6,11 @@ import java.util.Scanner;
 public class Advance1 {
 
 	public static void main(String[] args) {
-		System.out.println("学生の名前を入力してください");
-		
 		Scanner scanner = new Scanner(System.in);
+		
+		while(true) {
+		
+		System.out.println("学生の名前を入力してください");
 		
 		String name = scanner.nextLine();
 
@@ -28,15 +30,32 @@ public class Advance1 {
 		} else {
 			System.out.println(name +"さんの評価は1");
 		}
+		
+		System.out.println("入力を続けますか？（yes/no）");
+		
+		String continueString = scanner.nextLine();
 
-
-
+		if(continueString.equals("no")){
+		break;
+		}
+	}
+		  scanner.close();
+		
 	}
 
 }
 
 
-
+//# 練習2
+//- 繰り返し入力が出来るようにする
+//- 評価表示後に、下記を表示する
+//  - 「入力を続けますか？（yes/no）」
+//- yes以外が入力されるまで、名前と点数の入力を受け付ける
+//- 名前と点数の入力が終わったら、それぞれの評価の人数を表示する
+//  - 例：「5: X人」
+//- 補習が必要な人数を表示する
+//  - 「夏休みの補習の対象は、X人です」
+//  - 評価2以下で、補習の対象とする
 
 
 //# 練習1
@@ -50,3 +69,4 @@ public class Advance1 {
 //  - 50点以上: 〇〇さんの評価は3
 //  - 30点以上: 〇〇さんの評価は2
 //  - 30点未満: 〇〇さんの評価は1
+
